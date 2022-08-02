445a92ff6721ff032e01c22788d1c495463bf952
ben <revellben@hotmail.com>
2022-08-02 19:35:55 +0100

    end of July lesson

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import requests
 
 def main():
     print("hello world")
-    res = requests.get("http://www.google.com")
+    res = requests.get("http://127.0.0.1:8000")
+    print(res.headers)
     print(res.status_code)
     print(res.text)
+    #
+
 
 if __name__ == '__main__':
     main()
