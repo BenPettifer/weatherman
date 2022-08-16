7bfc05769a2bea8996abc6d7968c16c0b0c15e42
ben <revellben@hotmail.com>
2022-08-16 13:53:10 +0100

    update 22/10/08

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+from re import I
 import requests
 
-def main():
-    print("hello world")
-    res = requests.get("http://127.0.0.1:8000")
+def inputgetter():
+    text = input("is this cool?")
+    if text == "yes":
+        return True
+    else:
+        return False
+
+def APIpicker(isCool):
+    Starwarsapi = "https://swapi.dev/api/people/3"
+    PokemonAPI = "https://pokeapi.co/api/v2/pokemon/ditto"
+    URL = ""
+
+    if isCool:
+        URL = PokemonAPI
+    else:
+        URL = Starwarsapi
+    return URL
+
+def starwars_multiple_API_calls():
+    print("get first five")
+    for i in range(5):
+        res = requests.get("https://swapi.dev/api/people/"+str(i+1))
+        json = res.json() 
+        if int(json['height']) > 171:
+            templatestring = json['name'] + " " + json['height'] + " cm"
+            print(templatestring)
+            
+def normal(ChosenAPI):
+    print("requesting API")
+    res = requests.get(ChosenAPI)
     print(res.headers)
     print(res.status_code)
     print(res.text)
-    #
 
+def main():
+    isCool = inputgetter()
+    ChosenAPI = APIpicker(isCool)
+    if isCool == False:
+       starwars_multiple_API_calls()
+    else:
+        normal(ChosenAPI)
 
 if __name__ == '__main__':
     main()
